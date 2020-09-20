9171a3bae65bd3b0a47d967dd548339c7f91b884
Nata Pryhunova <prygunova@gmail.com>
2020-09-20 21:29:52 +0300

    дз 5

---
+++ js/task-03.js
@@ -0,0 +1,50 @@
+// Write code under this line
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    return this.items.push(item);
+  }
+  removeItem(item) {
+    return this.items.splice(this.items.indexOf(item), 1);
+  }
+}
+// console.log(typeof Storage);
+
+// 'function'
+
+const goods = ['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор'];
+
+const storage = new Storage(goods);
+
+// console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem('Дроид');
+// console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem('Пролонгер');
+// console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
